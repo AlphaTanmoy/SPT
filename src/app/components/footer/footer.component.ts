68ab25ad186459933f0de6a25b698f8adecd58ec
alphaTanmoy <dtanmoy169@gmail.com>
2025-04-26 07:24:21 +0530

    changes done for now

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [MatIconModule, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
-
-}
+export class FooterComponent { }
